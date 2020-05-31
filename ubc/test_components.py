3c29106e5af6ccae3ae6da6a00fd2716df1733a5
joaquin <j>
2020-05-31 15:07:50 -0700

    update regression test data

--- ubc/test_components.py
+++ ubc/test_components.py
@@ -1,10 +1,5 @@
-# this code has been automatically generated from pp/samples/pdk/write_tests.py
+# this code has been automatically generated from ubc/write_tests.py
 import ubc
-
-
-def test_bend_circular(data_regression):
-    c = ubc.bend_circular()
-    data_regression.check(c.get_settings())
 
 
 def test_y_splitter(data_regression):
@@ -12,11 +7,31 @@ def test_y_splitter(data_regression):
     data_regression.check(c.get_settings())
 
 
-def test_waveguide(data_regression):
-    c = ubc.waveguide()
+def test_gc_te1550(data_regression):
+    c = ubc.gc_te1550()
+    data_regression.check(c.get_settings())
+
+
+def test_bend90(data_regression):
+    c = ubc.bend90()
     data_regression.check(c.get_settings())
 
 
 def test_mzi(data_regression):
     c = ubc.mzi()
+    data_regression.check(c.get_settings())
+
+
+def test_mzi_te(data_regression):
+    c = ubc.mzi_te()
+    data_regression.check(c.get_settings())
+
+
+def test_ring_single_te(data_regression):
+    c = ubc.ring_single_te()
+    data_regression.check(c.get_settings())
+
+
+def test_waveguide(data_regression):
+    c = ubc.waveguide()
     data_regression.check(c.get_settings())
--- ubc/test_ports.py
+++ ubc/test_ports.py
@@ -1,9 +1,9 @@
-# this code has been automatically generated from pp/samples/pdk/write_tests.py
+# this code has been automatically generated from ubc/write_tests.py
 import ubc
 
 
-def test_bend_circular(num_regression):
-    c = ubc.bend_circular()
+def test_gc_te1550(num_regression):
+    c = ubc.gc_te1550()
     num_regression.check(c.get_ports_array())
 
 
@@ -12,11 +12,16 @@ def test_y_splitter(num_regression):
     num_regression.check(c.get_ports_array())
 
 
+def test_mzi(num_regression):
+    c = ubc.mzi()
+    num_regression.check(c.get_ports_array())
+
+
 def test_waveguide(num_regression):
     c = ubc.waveguide()
     num_regression.check(c.get_ports_array())
 
 
-def test_mzi(num_regression):
-    c = ubc.mzi()
+def test_bend90(num_regression):
+    c = ubc.bend90()
     num_regression.check(c.get_ports_array())
