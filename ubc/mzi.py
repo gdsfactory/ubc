163128e332ced97659d6ca4c97704ddc25db463d
joaquin <j>
2020-06-07 11:09:55 -0700

    adding some exercises

--- ubc/mzi.py
+++ ubc/mzi.py
@@ -5,12 +5,17 @@ from ubc.y_splitter import y_splitter
 
 
 @pp.autoname
-def mzi(delta_length=100):
+def mzi(
+    delta_length=100,
+    coupler_factory=y_splitter,
+    straight_factory=waveguide,
+    bend90_factory=bend_circular,
+):
     c = pp.c.mzi(
         L1=delta_length,
-        straight_factory=waveguide,
-        bend90_factory=bend_circular,
-        coupler_factory=y_splitter,
+        straight_factory=straight_factory,
+        bend90_factory=bend90_factory,
+        coupler_factory=coupler_factory,
     )
     return c
 
