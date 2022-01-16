3a30ca043e0e38a8391cca2708d43c8f4adf8358
joaquin <j>
2022-01-16 08:41:37 -0800

    get_install_requires

--- setup.py
+++ setup.py
@@ -12,12 +12,18 @@ def read(filename):
         return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
 
 
+def get_install_requires():
+    with open("requirements.txt", "r") as f:
+        return [line.strip() for line in f.readlines() if not line.startswith("-")]
+
+
 setup(
     name="ubc",
     version="0.0.6",
     url="https://github.com/gdsfactory/ubc",
     license="MIT",
     author="gdsfactory",
+    install_requires=get_install_requires(),
     author_email="https://github.com/gdsfactory/ubc",
     description="UBC Siepic Ebeam PDK from edx course",
     long_description=read("README.md"),
