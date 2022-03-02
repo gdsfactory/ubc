5b2141b108f35068a9588cf494cab1b15fe94578
joaquin <j>
2022-03-01 19:28:00 -0800

    markdown

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
-import io
-import os
-import re
-
 from setuptools import find_packages, setup
 
 
-def read(filename):
-    filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type(u"")
-    with io.open(filename, mode="r", encoding="utf-8") as fd:
-        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
 
 
 def get_install_requires():
@@ -26,7 +19,8 @@ setup(
     author="gdsfactory",
     install_requires=get_install_requires(),
     description="UBC Siepic Ebeam PDK from edx course",
-    long_description=read("README.md"),
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=("tests",)),
     python_requires=">=3.7",
     classifiers=[
