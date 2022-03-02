e0fa0ae7b0e56edfb0441b0dbe8e31cafed1e497
joaquin <j>
2022-03-01 16:34:31 -0800

    Bump version: 1.3.0 → 1.3.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_install_requires():
 
 setup(
     name="ubcpdk",
-    version="1.3.0",
+    version="1.3.1",
     url="https://github.com/gdsfactory/ubc",
     include_package_data=True,
     license="MIT",
