d98dd6ed5337dff6841794f819ccc5a0d59effd1
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-17 06:23:57 -0700

    Bump version: 1.4.2 → 1.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "ubcpdk"
-version = "1.4.2"
+version = "1.5.0"
 copyright = "2019, gdsfactory"
 author = "gdsfactory"
 
--- ubcpdk/__init__.py
+++ ubcpdk/__init__.py
@@ -16,7 +16,7 @@ from ubcpdk.tech import cross_sections
 
 gf.asserts.version(">=4.7.3")
 lys = gf.layers.load_lyp(PATH.lyp)
-__version__ = "1.4.2"
+__version__ = "1.5.0"
 
 __all__ = [
     "CONFIG",
