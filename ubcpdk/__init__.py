abe726c5d953275e0b4f78c9a02a5e936add010a
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-17 09:03:22 -0700

    register containers

--- ubcpdk/__init__.py
+++ ubcpdk/__init__.py
@@ -3,6 +3,7 @@ import gdsfactory as gf
 from gdsfactory.config import logger
 from gdsfactory.get_factories import get_cells
 from gdsfactory.pdk import Pdk
+from gdsfactory import containers
 
 
 from ubcpdk.config import CONFIG, PATH, module
@@ -34,7 +35,8 @@ __all__ = [
 
 
 logger.info(f"Found UBCpdk {__version__!r} installed at {module!r}")
-cells = get_cells(components)
+cells = get_cells([containers, components])
+_cells_to_test = get_cells(components)
 
 PDK = Pdk(name="ubcpdk", cells=cells, cross_sections=cross_sections)
 PDK.activate()
