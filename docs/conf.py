f39f537a6c6a82d7d5ed19333c990c4c1cebd19f
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-02 16:43:31 -0700

    Bump version: 1.5.10 → 1.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "ubcpdk"
-version = "1.5.10"
+version = "1.6.0"
 copyright = "2019, gdsfactory"
 author = "gdsfactory"
 
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_install_requires():
 
 setup(
     name="ubcpdk",
-    version="1.5.10",
+    version="1.6.0",
     url="https://github.com/gdsfactory/ubc",
     include_package_data=True,
     license="MIT",
