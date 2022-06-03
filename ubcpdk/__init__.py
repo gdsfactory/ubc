709f08586454ca4fa8933cabb051d258edcb9242
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-03 10:19:36 -0700

    Bump version: 1.6.0 → 1.6.1

--- ubcpdk/__init__.py
+++ ubcpdk/__init__.py
@@ -17,7 +17,7 @@ from ubcpdk.tech import cross_sections
 
 gf.asserts.version(">=5.1.2")
 lys = gf.layers.load_lyp(PATH.lyp)
-__version__ = "1.6.0"
+__version__ = "1.6.1"
 
 __all__ = [
     "CONFIG",
