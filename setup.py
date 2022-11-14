0d36ce5ff4aac9909f9ff2bdd64ed751f4077055
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-11-13 22:13:21 -0800

    Bump version: 1.8.0 → 1.9.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("requirements_simulations.txt") as f:
 
 setup(
     name="ubcpdk",
-    version="1.8.0",
+    version="1.9.0",
     url="https://github.com/gdsfactory/ubc",
     include_package_data=True,
     license="MIT",
