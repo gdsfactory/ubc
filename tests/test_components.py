56328e26dc722628507a4e37ab67cb6b69a33570
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-02 09:42:45 -0800

    move tests outside

---
+++ tests/test_components.py
@@ -0,0 +1,33 @@
+import pathlib
+import pytest
+from pytest_regressions.data_regression import DataRegressionFixture
+
+from gdsfactory.component import Component
+from gdsfactory.difftest import difftest
+from ubcpdk import cells
+
+
+skip_test = {"add_siepic_labels", "add_fiber_array_siepic"}
+cell_names = set(cells.keys()) - set(skip_test)
+dirpath = pathlib.Path(__file__).absolute().with_suffix(".gds")
+
+
+@pytest.fixture(params=cell_names, scope="function")
+def component(request) -> Component:
+    return cells[request.param]()
+
+
+def test_pdk_gds(component: Component) -> None:
+    """Avoid regressions in GDS geometry shapes and layers."""
+    difftest(component, dirpath=dirpath)
+
+
+def test_pdk_settings(
+    component: Component, data_regression: DataRegressionFixture
+) -> None:
+    """Avoid regressions when exporting settings."""
+    data_regression.check(component.to_dict())
+
+
+def test_assert_ports_on_grid(component: Component):
+    component.assert_ports_on_grid()
