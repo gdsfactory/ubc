2ffdd844f83cf6727913ae15c4a02c88027d81ef
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-02-04 21:00:23 -0800

    add examples for heater layouts including labels

--- ubcpdk/samples/ubc_joaquin_matres2.py
+++ ubcpdk/samples/ubc_joaquin_matres2.py
@@ -3,26 +3,45 @@
 import gdsfactory as gf
 
 import ubcpdk
+import ubcpdk.components as pdk
 from ubcpdk.tech import LAYER
 from ubcpdk.samples.write_mask import write_mask_gds_with_metadata
 
 size = (605, 410)
-add_gc = ubcpdk.components.add_fiber_array
 
 
 def test_mask3():
-    """Add MZI interferometers."""
+    """MZI interferometers."""
     mzi = gf.partial(gf.components.mzi, splitter=ubcpdk.components.ebeam_y_1550)
-    mzis = [mzi(delta_length=delta_length) for delta_length in [10, 100]]
-    mzis_with_gc = [add_gc(mzi) for mzi in mzis]
+    mzis = [mzi(delta_length=delta_length) for delta_length in [10, 40, 100]]
+    mzis_gc = [pdk.add_fiber_array(mzi) for mzi in mzis]
 
-    c = gf.pack(mzis_with_gc)
+    mzis = [pdk.mzi_heater(delta_length=delta_length) for delta_length in [40]]
+    mzis_heater_gc = [
+        pdk.add_fiber_array_pads_rf(mzi, optical_routing_type=2) for mzi in mzis
+    ]
+
+    c = gf.pack(mzis_gc + mzis_heater_gc, max_size=size)
     m = c[0]
     m.name = "EBeam_JoaquinMatres_3"
     m << gf.components.rectangle(size=size, layer=LAYER.FLOORPLAN)
     return write_mask_gds_with_metadata(m)
 
 
+def test_mask4():
+    """Ring resonators."""
+
+    rings = [pdk.ring_single_heater(length_x=length_x) for length_x in [4, 6]]
+    rings_gc = [pdk.add_fiber_array_pads_rf(ring) for ring in rings]
+
+    c = gf.pack(rings_gc, max_size=size)
+    m = c[0]
+    m.name = "EBeam_JoaquinMatres_4"
+    m << gf.components.rectangle(size=size, layer=LAYER.FLOORPLAN)
+    return write_mask_gds_with_metadata(m)
+
+
 if __name__ == "__main__":
-    m, tm = test_mask3()
+    # m, tm = test_mask3()
+    m, tm = test_mask4()
     m.show()
