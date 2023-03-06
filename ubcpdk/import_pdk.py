4d01cd46a832592c661c3fcc3c1d85b24e77d3a6
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-03-05 16:20:04 -0800

    fix docs

--- ubcpdk/import_pdk.py
+++ ubcpdk/import_pdk.py
@@ -5,11 +5,11 @@ from ubcpdk.config import PATH
 if __name__ == "__main__":
     print(
         gf.write_cells.get_import_gds_script(
-            PATH.gds / "EBeam", module="ubpdk.components"
+            dirpath=PATH.gds / "EBeam", module="ubpdk.components"
         )
     )
     print(
         gf.write_cells.get_import_gds_script(
-            "gds/EBeam_Beta", module="ubpdk.components"
+            dirpath="gds/EBeam_Beta", module="ubpdk.components"
         )
     )
