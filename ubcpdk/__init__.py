677377efd54e2f267f1be9d870d43829a37a9dd4
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-08-05 20:45:28 -0700

    Bump version: 1.21.4 → 2.0.0

--- ubcpdk/__init__.py
+++ ubcpdk/__init__.py
@@ -10,7 +10,7 @@ from ubcpdk import components, data, tech
 from ubcpdk.config import CONFIG, PATH, module
 from ubcpdk.tech import LAYER, LAYER_STACK, LAYER_VIEWS, cross_sections, strip
 
-__version__ = "1.21.4"
+__version__ = "2.0.0"
 
 __all__ = [
     "CONFIG",
