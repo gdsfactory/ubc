aa03bdcb3a6a7817313a836f55101f9018223e15
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-10-01 11:37:27 -0700

    Bump to 2.2.1

--- ubcpdk/__init__.py
+++ ubcpdk/__init__.py
@@ -11,7 +11,7 @@ from ubcpdk import components, data, tech
 from ubcpdk.config import CONFIG, PATH, module
 from ubcpdk.tech import LAYER, LAYER_STACK, LAYER_VIEWS, cross_sections
 
-__version__ = "2.2.0"
+__version__ = "2.2.1"
 
 __all__ = [
     "CONFIG",
