877b76ef6d8468cdddf1c8be9aea473284e9926f
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-09-02 10:21:46 -0700

    set meta

--- ubcpdk/import_gds.py
+++ ubcpdk/import_gds.py
@@ -14,6 +14,9 @@ from ubcpdk.tech import LAYER
 def kcell_to_component(kcell: kf.kcell.ProtoTKCell[Any]) -> Component:
     kcell.set_meta_data()
 
+    for ci in kcell.called_cells():
+        kcell.kcl[ci].set_meta_data()
+
     c = Component()
     c.name = kcell.name
     c.kdb_cell.copy_tree(kcell.kdb_cell)
