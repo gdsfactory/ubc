2234c31db6eb28c3eab59b49bda92c9667c87ec6
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-09-06 15:32:14 +0200

    fix docs and tests

--- ubcpdk/cells/mzis.py
+++ ubcpdk/cells/mzis.py
@@ -10,6 +10,66 @@ from gdsfactory.typings import ComponentSpec, CrossSectionSpec
 
 
 @gf.cell
+def mzi_1x1(
+    delta_length: float = 10,
+    bend: ComponentSpec = "bend_euler",
+    straight: ComponentSpec = "straight",
+    splitter: ComponentSpec = "ebeam_y_1550",
+    combiner: ComponentSpec | None = None,
+    port_e1_splitter: str = "o2",
+    port_e0_splitter: str = "o3",
+    port_e1_combiner: str = "o2",
+    port_e0_combiner: str = "o3",
+    cross_section: CrossSectionSpec = "strip",
+) -> gf.Component:
+    """Mzi.
+
+    Args:
+        delta_length: bottom arm vertical extra length.
+        bend: 90 degrees bend library.
+        straight: straight function.
+        splitter: splitter function.
+        combiner: combiner function.
+        port_e1_splitter: east top splitter port.
+        port_e0_splitter: east bot splitter port.
+        port_e1_combiner: east top combiner port.
+        port_e0_combiner: east bot combiner port.
+        cross_section: for routing (sxtop/sxbot to combiner).
+    """
+    combiner = combiner or splitter
+    _splitter = gf.get_component(splitter)
+    _combiner = gf.get_component(combiner)
+
+    return gf.c.mzi(
+        delta_length=delta_length,
+        bend=bend,
+        straight=straight,
+        splitter=splitter,
+        combiner=combiner,
+        port_e1_splitter=port_e1_splitter,
+        port_e0_splitter=port_e0_splitter,
+        port_e1_combiner=port_e1_combiner,
+        port_e0_combiner=port_e0_combiner,
+        cross_section=cross_section,
+        length_y=2.0,
+        length_x=0.1,
+        straight_y=None,
+        straight_x_top=None,
+        straight_x_bot=None,
+        with_splitter=True,
+        port1="o1",
+        port2="o2",
+        nbends=2,
+        cross_section_x_top=None,
+        cross_section_x_bot=None,
+        mirror_bot=False,
+        add_optical_ports_arms=False,
+        min_length=0.01,
+        auto_rename_ports=True,
+    )
+
+
+@gf.cell
 def mzi(
     delta_length: float = 10,
     bend: ComponentSpec = "bend_euler",
@@ -109,5 +169,5 @@ if __name__ == "__main__":
     from ubcpdk import PDK
 
     PDK.activate()
-    c = mzi_heater(length_x=100)
+    c = mzi_1x1()
     c.show()
